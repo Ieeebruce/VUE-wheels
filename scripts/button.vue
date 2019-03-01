a1ac78a385ace7a4a3fda2f35d2ecbc5a65bf2d2
lht <744252320@qq.com>
2019-03-01 13:03:10 +0800

    单文件组件

---
+++ scripts/button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="g-button">hello</button>
+</template>
+<script>
+export default {};
+</script>
+<style lang='scss'>
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-background);
+  &:hover {
+    border-color: var(--button-color-hover);
+  }
+  &:active {
+    background: var(--button-active-background);
+  }
+}
+</style>
+
