06d5556b8d32722ead55c6b48dacc7f2040c8faa
lht <744252320@qq.com>
2019-03-01 17:17:16 +0800

    添加icon/icon-position选择icon位置/

--- dist/app.c09d0a7b.js
+++ dist/app.c09d0a7b.js
@@ -12364,7 +12364,26 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+//
+var _default = {
+  // props: ["icon", "iconPosition"]
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator: function validator(value) {
+        return value === "left" || value === "right";
+      }
+    }
+  }
+};
 exports.default = _default;
         var $ab3764 = exports.default || module.exports;
       
@@ -12378,7 +12397,19 @@ exports.default = _default;
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "g-button" }, [_vm._v("hello")])
+  return _c(
+    "button",
+    { staticClass: "g-button", class: "icon-" + _vm.iconPosition },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12454,7 +12485,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63205" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59926" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- scripts/button.vue
+++ scripts/button.vue
@@ -1,12 +1,37 @@
 <template>
-  <button class="g-button">hello</button>
+  <button class="g-button" v-bind:class="`icon-${iconPosition}`">
+    <svg v-if="icon" class="icon">
+      <use v-bind:xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {};
+export default {
+  // props: ["icon", "iconPosition"]
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      }
+    }
+  }
+};
 </script>
 <style lang='scss'>
 .g-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: top;
+  outline: none;
   font-size: var(--font-size);
+  line-height: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
   border-radius: var(--border-radius);
@@ -17,6 +42,23 @@ export default {};
   }
   &:active {
     background: var(--button-active-background);
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-left: 0.3em;
+      margin-right: 0;
+    }
   }
 }
 </style>
