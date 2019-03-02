68d8ddedfb0b89cf18b0777afd155043a4662a25
lht <744252320@qq.com>
2019-03-02 12:50:48 +0800

    button-group

---
+++ scripts/button-group.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.button-group {
+  display: inline-flex;
+  vertical-align: top;
+  > .g-button {
+    border-radius: 0;
+    &:not(:first-child) {
+      margin-left: var(--border-width-);
+    }
+    &:last-child {
+      border-bottom-right-radius: var(--border-radius);
+      border-top-right-radius: var(--border-radius);
+    }
+    &:first-child {
+      border-bottom-left-radius: var(--border-radius);
+      border-top-left-radius: var(--border-radius);
+    }
+    &:hover,
+    &:active {
+      z-index: 1;
+    }
+  }
+}
+</style>
